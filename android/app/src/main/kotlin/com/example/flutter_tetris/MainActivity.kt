2b9e855d5e72f96f3fda3a5371e7bce898571b37
boyan01 <17426470+boyan01@users.noreply.github.com>
2025-10-04 14:52:57 +0800

    re-gen android platform files

--- android/app/src/main/kotlin/com/example/flutter_tetris/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_tetris
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/tetris/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tetris
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
