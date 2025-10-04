8c3eb00b8bc65bce783d461bb868a70c947dbf81
boyan01 <boyan01@users.noreply.github.com>
2025-10-04 10:33:45 +0000

    Automated deployment: Sat Oct  4 10:33:45 UTC 2025 25354c4636b6b84fa6746f85e364239904e011fc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3663613870"
+    serviceWorkerVersion: "3598496202"
   }
 });
